f88b153b6f21b3f61d3dffc283592cd65367abf4
Bernardo <service.bsfg@gmail.com>
2024-07-28 11:45:11 -0500

    working with objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// Initial employee object
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+  
+  function updateEmployeeWithKeyAndValue(empObj, key, value) {
+    
+    const newObj = {...empObj}
+    newObj[key] = value
+    return newObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(empObj, key, value) {
+    
+    empObj[key] = value
+    return empObj;
+}
+
+function deleteFromEmployeeByKey(empObj, key){
+    const newObj = {...empObj}
+    delete newObj[key]
+    return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(empObj, key){
+    delete empObj[key]
+    return empObj
+}
